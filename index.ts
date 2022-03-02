08a693040ddfb74d7baa27cbe03cb3354d1718eb
Kenramiscal1106 <ramiscalken@gmail.com>
2022-03-03 07:04:07 +0800

    Initialized repository

---
+++ index.ts
@@ -0,0 +1,22 @@
+let helloWorld: string = 'user';
+type user = {
+  name:string,
+  age: number,
+} | [
+  string,
+  number,
+  ...any
+]
+interface ui {
+  name: string,
+  age: number
+}
+let user1: ui = {
+  age:34,
+  name:'ken'
+}
+let user2: user = {
+  name:'ken',
+  age:15
+}
+let user3: user = ['Ken', 18, 'this']
