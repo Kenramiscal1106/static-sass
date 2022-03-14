8939e19018029fcb113334d1b6526bba4e9c6263
Kenramiscal1106 <ramiscalken@gmail.com>
2022-03-14 11:19:54 +0800

    Modified file: - recompiled

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-"use strict";
-let helloWorld = 'user';
-let user1 = {
-    age: 34,
-    name: 'ken'
-};
-let user2 = {
-    name: 'ken',
-    age: 15
-};
-let user3 = ['Ken', 18, 'this'];
+var helloWorld = "user";
